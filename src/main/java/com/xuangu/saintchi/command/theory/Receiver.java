a42cba68300ab5fdfa37662611ecb6775628ed66
Saintchi <shengzhipan@qq.com>
2017-02-21 22:47:30 +0800

    Story:设计模式 Issue: Remarks:第二次提交，第一次更新测试。 Author: xuangu

--- src/main/java/com/xuangu/saintchi/command/theory/Receiver.java
+++ src/main/java/com/xuangu/saintchi/command/theory/Receiver.java
@@ -3,7 +3,7 @@ package com.xuangu.saintchi.command.theory;
 public class Receiver {
 
 	public void action() {
-		System.out.println("Receive something, run action");
+		System.out.println("Receive something, run action.");
 	}
-
+	
 }
